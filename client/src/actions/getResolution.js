c60801df1a4bccff35d7f0580130fc897a69682e
Prashant Kumar <prash.kumar047@gmail.com>
2019-01-23 19:22:05 +0530

    updated the chat-box ui and working

--- client/src/actions/getResolution.js
+++ client/src/actions/getResolution.js
@@ -1,21 +1,36 @@
 import axios from 'axios';
 import { GET_ERRORS, GET_AND_SAVE_USER_QUERY, GET_AND_SAVE_BOT_REPLY, SAVE_CHAT, LOADING_RESULTS } from './types';
 
-export const getResolution = (data) => (dispatch) => {
-	dispatch({type:LOADING_RESULTS});
-	dispatch({type:GET_AND_SAVE_USER_QUERY,payload:data.text});
-	axios.post('/api/getResolution', data)
-		.then(res => {
-			dispatch({
-				type:GET_AND_SAVE_BOT_REPLY,
-				payload:res.data.reply});
-			dispatch({
-				type:SAVE_CHAT,
-				payload:{user:data.text,reply:res.data.reply}});
-		})
-		.catch(err => 
-			dispatch({
-				type:GET_ERRORS,
-				payload:(err.response)?err.response.data:"Unknown Error Occured"})
-		);
+
+export const getResolution = (data, dispatch) => {
+	dispatch((dispatcher) => {
+		dispatcher({type:LOADING_RESULTS});
+		dispatcher({type:GET_AND_SAVE_USER_QUERY, payload:data.message});
+		dispatcher({
+			type:SAVE_CHAT,
+			payload:{mtag:'CLIENT', message: data.message}
+		});
+
+		
+		setTimeout(() => {
+			axios.post('/api/getResolution', data)
+				.then(res => {
+					dispatcher({
+						type:GET_AND_SAVE_BOT_REPLY,
+						payload:res.data.reply
+					});
+					dispatcher({
+						type:SAVE_CHAT,
+						payload:{mtag:'SERVER',message:res.data.reply}
+					});
+				})
+				.catch(err => 
+					dispatcher({
+						type:GET_ERRORS,
+						payload:(err.response)?err.response.data:"Unknown Error Occured"})
+				);
+
+		},1000)
+		
+	});
 }
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,72 +1,15 @@
-//Libraries
-import React from 'react';
+import React, { Component } from 'react';
 
-//Components
-import Chatbot from '../../templates/chatbot/chatbot'
-import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import Icon from '@material-ui/core/Icon';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
+import ChatBox from '../chatbot/ChatBox';
 
-const styles = theme => ({
-    root: {
-        position: 'relative',
-        overflow: 'hidden',
-    },
-    button: {
-        position: 'fixed',
-        bottom: theme.spacing.unit * 2,
-        right: theme.spacing.unit * 2,
-    },
-    chatbot: {
-        position: 'fixed',
-        bottom: theme.spacing.unit * 2,
-        padding: theme.spacing.unit,
-        right: 100,
-        width: 300,
-        height: 500,
-    },
-})
-
-class Home extends React.Component {
-    state = {
-        open: false,
-    }
-
-    openChat = () => {
-        this.setState({ 
-            open: !this.state.open,
-        })
-    };
-
-    closeChat = (state) => {
-        this.setState({
-            open: state
-        });
-    };
-
-    render() {
-        const { classes } = this.props;
+class Home extends Component {
+	render(){
         return(
             <div>
-                <Button className={classes.button} onClick={this.openChat}>
-                    <Fab color="primary" aria-label="Add" className={classes.fab}>
-                        <Icon>chat</Icon>
-                    </Fab>
-                </Button>
-                {this.state.open ?
-                    <Paper className={classes.chatbot} elevation={10}>
-                        <Chatbot
-                            isOpen = {this.state.open}
-                            updateState = {newState => this.closeChat(newState)}
-                        />  
-                    </Paper> 
-                    :null
-                }
+                <ChatBox />
             </div>
         );
     }
 }
 
-export default withStyles(styles)(Home);
+export {Home};
