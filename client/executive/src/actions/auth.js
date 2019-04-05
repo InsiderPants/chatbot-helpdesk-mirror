a1d2058b93657d8dde2d269afe71707d1a77b08e
Ajay Dabas <se.mailforwork@gmail.com>
2019-04-05 23:28:44 +0530

    minor mistake in auth.js fixed

--- client/executive/src/actions/auth.js
+++ client/executive/src/actions/auth.js
@@ -47,7 +47,7 @@ export const setCurrentUser = (decoded)=>{
 }
 
 //action for executive logout
-export const signOutUser = (dispatch) => {
+export const signOutUser = () => (dispatch) => {
     dispatch({
         type: LOGOUT_EXECUTIVE,
     });
