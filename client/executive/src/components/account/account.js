9f20202df7eb8598e85ffb060f67cb65b6c41d09
Ajay Dabas <se.mailforwork@gmail.com>
2019-04-14 15:51:26 +0530

    changes in accounts page executive

--- client/executive/src/components/account/account.js
+++ client/executive/src/components/account/account.js
@@ -10,8 +10,12 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from "@material-ui/core/Divider";
 import IconButton from '@material-ui/core/IconButton';
+import Fab from '@material-ui/core/Fab';
 import Icon from '@material-ui/core/Icon';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import classNames from 'classnames';
 
 import Navbar from '../navbar/navbar';
 
@@ -35,6 +39,24 @@ const styles = theme => ({
         marginTop: theme.spacing.unit,
         marginLeft: theme.spacing.unit * 3,
         padding: theme.spacing.unit,
+        backgroundColor: '#104087',
+        color: 'white',
+        '&:hover': {
+            backgroundColor: '#08254f'
+        }
+    },
+    fab: {
+        margin: theme.spacing.unit,
+        backgroundColor: '#104087',
+        '&:hover': {
+            backgroundColor: '#08254f'
+        }
+    },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
+    iconSmall: {
+        fontSize: 20,
     },
 })
 
@@ -43,15 +65,13 @@ class Account extends React.Component {
         super();
         // change every string to empty 
         this.state = {
-            name: 'Manish',     // name of executive
-            contact: '9876543210',    // contact of executive
+            contact: '12345789',    // contact of executive
             changeContact: false,   // for change contact functionality
-            email: 'm@m.com',   // email of executive
             oldPassword: '',    // for old password textfield
             newPassword: '',   // actives on when change password clicked
-            cnofirmNewPassword: '',     // for confirm password textfield
+            confirmNewPassword: '',     // for confirm password textfield
             changePassword: false,  // for change passowrd button and fuctions
-            queryResolved: '20',    // just a counter for number of query resolved by this executive
+            queryResolved: '',    // just a counter for number of query resolved by this executive
         };
     }
 
@@ -76,6 +96,27 @@ class Account extends React.Component {
         })
     }
 
+    // contact textfield change
+    handleContactChange = (e) => {
+        this.setState({
+            contact: e.target.value,
+        })
+    }
+
+    // edit contact
+    editContact = (e) =>{
+        this.setState({
+            changeContact: true,
+        })
+    }
+
+    // save contact
+    saveContact = (e) =>{
+        this.setState({
+            changeContact: false,
+        })
+    }
+
     render() {
         const {classes} = this.props;
 
@@ -91,57 +132,76 @@ class Account extends React.Component {
             <Paper elevation={1} style={{height: 800, padding: 8}}>
                 {/* Name or Change Password Heading*/}
                 <Typography align='left' color='textPrimary' variant='display3' style={{margin:20, fontWeight: 200}}>
-                    {this.state.changePassword ? "Change Password" : this.state.name}
+                    {this.state.changePassword ? "Change Password" : this.props.executive.name}
                 </Typography>
                 <Divider />
-                {this.state.changePassword  //conditional rendering for change password
-                    ?
-                    <Grid container justify='flex-start' alignItems='center' spacing={8}>
-                        <Grid item xs={6}>
-                            {/* for 3 password inputs for change password */}
-                            <div style={{marginTop: 20, marginLeft: 20}}>
-                                {/* old password */}
-                                <TextField type="password" placeholder="Enter Old Password" label="Old Password" required fullWidth value={this.state.oldPassword} onChange={this.handleOldPasswordChange}/>
-                                <br/>
-                                <br/>
-                                {/* new password */}
-                                <TextField type="password" placeholder="Enter New Password" label="New Password" required fullWidth value={this.state.newPassword} onChange={this.handleNewPasswordChange}/>
-                                <br/>
-                                <br/>
-                                {/* confirm new password */}
-                                <TextField type="password" placeholder="Confirm New Password" label="Confirm New Password" required fullWidth value={this.state.confirmNewPassword} onChange={this.handleConfirmPasswordChange}/>
-                            </div>
-                        </Grid>
-                        <Grid item xs={6}/>
-                    </Grid>
-                    :
                     <div>
                         {/* Email */}
                         <Typography align='left' color='textPrimary' variant='h5' style={{ margin: 20, fontWeight: 300 }}>
-                            E-mail:    {this.state.email}
+                            E-mail:    {this.props.executive.email}
                         </Typography>
-                        {/* Conatct */}
+                        {/* Contact */}
                         <Grid container justify='flex-start' alignItems='center' spacing={8}>
                             <Grid item xs='auto'>
-                                <Typography align='left' color='textPrimary' variant='h5' style={{ marginLeft: 20, fontWeight: 300 }}>
-                                    Contact: {this.state.contact}
-                                </Typography>
+                                {this.state.changeContact //conditional rendering for change contact
+                                    ?
+                                    <div>
+                                    <Typography align='left' color='textPrimary' variant='h5' style={{ marginLeft: 20, fontWeight: 300 }}>
+                                        Contact: 
+                                    </Typography>
+                                    <TextField type="text" placeholder={this.state.contact} label="Contact" required fullWidth value={this.state.contact} onChange={this.handleContactChange}/>    
+                                    </div>
+                                    :
+                                    <Typography align='left' color='textPrimary' variant='h5' style={{ marginLeft: 20, fontWeight: 300 }}>
+                                        Contact: {this.state.contact}
+                                    </Typography>
+                                }
                             </Grid>
                             <Grid item xs={2}>
-                                {/* edit button */}
-                                <IconButton>
-                                    <Icon>
-                                        edit
-                                    </Icon>
-                                </IconButton>
+                                {this.state.changeContact //conditional rendering for change contact
+                                    ?
+                                    <Fab size="medium" color="secondary" aria-label="Edit" onClick={this.saveContact} className={classes.fab}>
+                                        <Icon>done_all</Icon>
+                                    </Fab>
+                                    :
+                                    <Fab size="medium" color="secondary" aria-label="Edit" onClick={this.editContact} className={classes.fab}>
+                                        <Icon>edit_icon</Icon>
+                                    </Fab>
+                                }
                             </Grid>
+                        </Grid>
+                        <Grid container justify='flex-start' alignItems='center' spacing={8}>
+                            <Grid item xs={6}>
+                                <Typography align='left' color='textPrimary' variant='h5' style={{ margin: 20, fontWeight: 300 }}>
+                                    Change Password:
+                                </Typography>
+                                {/* for 3 password inputs for change password */}
+                                <div style={{marginTop: 20, marginLeft: 20}}>
+                                    {/* old password */}
+                                    <TextField type="password" label="Old Password" required fullWidth value={this.state.oldPassword} onChange={this.handleOldPasswordChange}/>
+                                    <br/>
+                                    <br/>
+                                    {/* new password */}
+                                    <TextField type="password" label="New Password" required fullWidth value={this.state.newPassword} onChange={this.handleNewPasswordChange}/>
+                                    <br/>
+                                    <br/>
+                                    {/* confirm new password */}
+                                    <TextField type="password" label="Confirm New Password" required fullWidth value={this.state.confirmNewPassword} onChange={this.handleConfirmPasswordChange}/>
+                                </div>
+                                <br/>
+                                <Button variant="contained" size="small" className={classes.button}>
+                                    <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
+                                        Save New Password
+                                </Button>
+                                <br/>
+                            </Grid>
+                            <Grid item xs={6}/>
                         </Grid>
                         {/* query resolved */}
                         <Typography align='left' color='textPrimary' variant='h5' style={{ margin: 20, fontWeight: 300 }}>
                             Query Resolved: {this.state.queryResolved}
                         </Typography>
                     </div>
-                }
             </Paper>
         );
 
@@ -159,7 +219,7 @@ class Account extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-
+        executive: state.executive
     };
 }
 
